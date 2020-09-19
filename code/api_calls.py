cd494971343a5ba2dd0398c25a92fc52f5446c0f
Mikkel Schöttner <mikkel.schoettner@yahoo.de>
2020-09-19 02:28:17 +0200

    streamline code

--- code/api_calls.py
+++ code/api_calls.py
@@ -2,5 +2,5 @@ import requests
 user = 'Bearer'
 token = '8c35a7f0a5cf41a8d7e4c924e60dc7a04f08d006'
 response = requests.get('https://api-beta.bite.ai/meals/',
-                        headers = {'Authorization': 'Bearer 8c35a7f0a5cf41a8d7e4c924e60dc7a04f08d006'})
-print(response)
+                        headers = {'Authorization': f'{user} {token}'})
+print(response.text)
