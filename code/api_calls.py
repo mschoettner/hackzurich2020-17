d4255eb1233cf2e8a50acae0cc89ff9c163ce6c2
Mikkel Schöttner <mikkel.schoettner@yahoo.de>
2020-09-19 15:07:44 +0200

    Changed code...

--- code/api_calls.py
+++ code/api_calls.py
@@ -5,10 +5,10 @@ import json
 
 user = 'Bearer'
 token = '8c35a7f0a5cf41a8d7e4c924e60dc7a04f08d006'
-headers1 = {'Authorization': f'{user} {token}'}
-r1 = requests.get('https://api-beta.bite.ai/meals/',
-                        headers = headers1)
-#print(r1.text)
+# headers1 = {'Authorization': f'{user} {token}'}
+# r1 = requests.get('https://api-beta.bite.ai/meals/',
+#                         headers = headers1)
+# #print(r1.text)
 def food_recognition(path, user, token):
     """Food recognition request with picture. Takes path to image and returns
     annotations."""
@@ -31,15 +31,24 @@ def food_recognition(path, user, token):
 
     r2 = requests.post(url = food_rec_url, json = data, headers = headers2)
 
-    output = json.loads(r2.text)
-    food_items = []
-    for item in output['items']:
-        print(item['item']['name'], '\nchildren:')
+    image_data = json.loads(r2.text)
+    food_items = {}
+    for item in image_data['items']:
+        children = []
+        name = item['item']['name']
+        #print(name, '\nchildren:')
         for child in item['item']['children']:
-            print('\t',child['name'])
-        print('\n')
-    #print(output['items'][0]['score'])
-    return(output)
+            children.append(child['name'])
+            #print('\t',child['name'])
+        food_items[name] = children
+        #print('\n')
+    print(food_items)
+    return(food_items, image_data)
+
+def nutrition_values(image_data, subclasses):
+    # this somehow needs to get the user input for each subclass
+    # get calories and macronutrients for chosen subclasses
+    pass
 
 
-food_recognition("/mnt/c/hackzurich2020-17/media/meat.JPG", user = user, token = token)
+food_items, image_data = food_recognition("/mnt/c/hackzurich2020-17/media/meat.JPG", user = user, token = token)
